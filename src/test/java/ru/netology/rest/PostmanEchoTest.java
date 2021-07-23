024a0cfeebb40c403eb2c5f49f356013016f744d
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-07-23 17:15:30 +0300

    rollback to correct equal

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Alexandra Matyukhina"))
+                .body("data", equalTo("Matyukhina Alexandra"))
         ;
     }
 
