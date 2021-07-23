ad5012fc3c7b74289a2ba5346546d5e7cb934d73
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-07-23 17:08:15 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/789954wjsdcgk1kn?svg=true)](https://ci.appveyor.com/project/Alexandra-Matyukhina/postmanecho)
---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldCheckPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Matyukhina Alexandra")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Matyukhina Alexandra"))
+        ;
+    }
+
+}
